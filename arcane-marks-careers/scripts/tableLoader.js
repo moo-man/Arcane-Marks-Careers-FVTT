c853175e6d1e28910f88d87208fff51f0f336976
Russell <russellthurman94@gmail.com>
2019-08-21 22:56:43 -0500

    Delete tableLoader.js

--- arcane-marks-careers/scripts/tableLoader.js
+++
@@ -1,9 +0,0 @@
-function loadTables() {
-
-game.socket.emit("getFiles", "modules/arcane-marks-careers/tables", {}, resp => {
-    for (var file of resp.files)
-    {
-      console.log(file);
-    }
-  })
-}
