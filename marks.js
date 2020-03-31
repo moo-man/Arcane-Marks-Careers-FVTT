0e2011fdbd003680008b46705a79321c52206929
Russell <russellthurman94@gmail.com>
2020-03-30 22:40:37 -0500

    Add mark rolling option

--- marks.js
+++ marks.js
@@ -1,4 +1,17 @@
+Hooks.on("init", () => {
+	game.settings.register("arcane-marks-careers", "rollMarks", {
+		name : "Automatically Roll for Arcane Marks",
+		hint : "When Critical Casting or Critical Channelling, the system will also roll a d10, if the result is an 8, it displays a link to roll on the Arcane Marks table.",
+		scope : "world",
+		config : true,
+		default : true,
+		type : Boolean
+	});
+})
+
 Hooks.on("wfrp4e:rollCastTest", testResults => {
+    if (!game.settings.get("arcane-marks-careers", "rollMarks"))
+        return
     if (testResults.extra.critical)
     {
         if (new Roll("1d10").roll().total == 8)
@@ -13,6 +26,8 @@ Hooks.on("wfrp4e:rollCastTest", testResults => {
 })
 
 Hooks.on("wfrp4e:rollChannelTest", testResults => {
+    if (!game.settings.get("arcane-marks-careers", "rollMarks"))
+        return
     if (testResults.extra.criticalchannell)
     {
         if (new Roll("1d10").roll().total == 8)
